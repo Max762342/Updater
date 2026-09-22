834ee851fd1d144fe6fa3c4922bc3e693659bd71
Max762342 <hhsbansn@gmail.com>
2026-09-22 13:17:30 +0200

    Update oracle_server.py

--- oracle_server.py
+++ oracle_server.py
@@ -9,7 +9,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
 HOST = os.environ.get("ORACLE_HOST", "0.0.0.0")
-PORT = int(os.environ.get("ORACLE_PORT", "8080"))
+PORT = int(os.environ.get("ORACLE_PORT", "7777"))
 API_KEY = os.environ.get("ORACLE_API_KEY", "sychos-oracle-2024")
 ADMIN_KEY = os.environ.get("ORACLE_ADMIN_KEY", "sychos-admin-2024")
 DB_PATH = os.environ.get("ORACLE_DB", "sychos.db")
